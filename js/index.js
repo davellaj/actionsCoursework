cd967e479615e508283de723d9facbc014273bb0
davellaj <jamie.davella@gmail.com>
2016-12-12 14:28:18 -0500

    complete coursework example

--- js/index.js
+++ js/index.js
@@ -1,11 +1,7 @@
 require('babel-polyfill');
 
-import * as reducers from './reducers/index'
-
-import * as actions from './actions/index'
-
+import * as actions from './actions/index';
 import store from './store';
 
 store.dispatch(actions.addRepository('joe'));
 console.log(store.getState());
-
